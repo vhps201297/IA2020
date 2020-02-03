c6e539e9e4bd7e786a171b2b266b6615bc1d2e95
EnriqueGuerreroL <chikehp710@yahoo.com>
2020-02-03 10:27:11 -0600

    [ADD] Derivative - vector

--- Programa2/Polinomio.py
+++ Programa2/Polinomio.py
@@ -14,11 +14,21 @@ class polynomial():
     @property
     def degree(self):
         return len(self.expr) - 1
-    
+
+    @property
+    def derivative(self):
+        # menos_uno = self.expr[:len(self.expr) - 2]
+        # menos_uno = [term -1 for term in menos_uno]
+        derivative_v = [term -1 for term in self.expr[:-2]]
+        derivative_v.append(self.expr[-2])
+        return derivative_v
+
 # términos del polinomio
-terminos = [3,2,1,9]
-# se instancia polynomial
+terminos = [35,6,2,1,9]
+# se instancia polinomio
 polinomio = polynomial()
 polinomio.expression = terminos
-#print(polinomio.expr)
-print(polinomio.degree)
+
+print("\npolinomio: ", polinomio.expr)
+print("grado del polinomio: ", polinomio.degree)
+print("derivada: ", polinomio.derivative, "\n")
