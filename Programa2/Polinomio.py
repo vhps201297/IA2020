234f8734ad05939e9a46135640a74f936406d016
EnriqueGuerreroL <chikehp710@yahoo.com>
2020-02-03 13:02:22 -0600

    [ADD] Integral - vector

--- Programa2/Polinomio.py
+++ Programa2/Polinomio.py
@@ -1,3 +1,13 @@
+from fractions import Fraction
+def mcd(a,b):
+    #   assert type(a) == int and type(b) == int and a>0 and b>0
+    if a%b != 0:
+        print("return mcd: ", mcd(b,a%b))
+        return mcd(b,a%b)
+    else:
+        print("return b: ", b)
+        return b
+
 class polynomial():
     
     def __init__(self):
@@ -23,12 +33,22 @@ class polynomial():
             der.append(vector_rev[term] * term)
         return der[::-1]
 
+    @property
+    def integral(self):
+        integral_v = []
+        vector_rev = self.expression[::-1]
+        for term in range(len(vector_rev)):
+            integral_v.append(str(Fraction(str(vector_rev[term])+ "/" + str(term + 1))))
+        integral_v.insert(0, '0')
+        return integral_v[::-1]
+
+
 # términos del polinomio
-terms = [7,9,2,1,9]
-# se instancia polinomio
+terms = [8,0]
 polinomio = polynomial()
 polinomio.expression = terms
 
 print("\npolinomio: ", polinomio.expression)
 print("grado del polinomio: ", polinomio.degree)
-print("derivada: ", polinomio.derivative, "\n")
+print("derivada: ", polinomio.derivative)
+print("integral: ", polinomio.integral, "\n")
