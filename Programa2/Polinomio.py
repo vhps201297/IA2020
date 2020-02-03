fa0050e118d6b6b24391ea7b09f92bc3fdeb50b7
EnriqueGuerreroL <chikehp710@yahoo.com>
2020-02-03 15:05:13 -0600

    [ADD] Polynomial - "Pretty printing"

--- Programa2/Polinomio.py
+++ Programa2/Polinomio.py
@@ -19,11 +19,11 @@ class polynomial():
 
     @property
     def derivative(self):
-        der = []
+        derivative_v = []
         vector_rev = self.expression[::-1]
         for term in range(1, len(vector_rev)):
-            der.append(vector_rev[term] * term)
-        return der[::-1]
+            derivative_v.append(vector_rev[term] * term)
+        return derivative_v[::-1]
 
     @property
     def integral(self):
@@ -34,12 +34,11 @@ class polynomial():
         integral_v.insert(0, '0')
         return integral_v[::-1]
 
-    @property
-    def integral_pp(self):
-        vector_rev = self.integral[::-1]
+    def pretty_printing(self, vector):
+        vector_rev = vector[::-1]
         pretty_printing = ''
         for term in range(len(vector_rev)-1, -1, -1):
-            pretty_printing += "(" + str(vector_rev[term]) + ")" +"x^" + str(term) + " + "
+            pretty_printing += "(" + str(vector_rev[term]) + ")" + "x^" + str(term) + " + "
         return pretty_printing[:-3]
 
 # términos del polinomio
@@ -47,8 +46,10 @@ terms = [3,3,2,5]
 polinomio = polynomial()
 polinomio.expression = terms
 
-print("\npolinomio: ", polinomio.expression)
-print("grado del polinomio: ", polinomio.degree)
+print("\npolinomio (vector): ", polinomio.expression)
+print("polinomio: ", polinomio.pretty_printing(polinomio.expression), "\n")
+print("grado del polinomio: ", polinomio.degree, "\n")
 print("derivada: ", polinomio.derivative)
+print("derivada (polinomio): ", polinomio.pretty_printing(polinomio.derivative), "\n")
 print("integral (vector): ", polinomio.integral)
-print("integral (polinomio): ", polinomio.integral_pp, "\n")
+print("integral (polinomio): ", polinomio.pretty_printing(polinomio.integral), "\n")
